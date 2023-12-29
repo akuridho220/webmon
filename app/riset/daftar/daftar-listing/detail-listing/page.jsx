2326612f6f203888b8f86532a1739b138c5bcc7c
akuridho220 <akuridho220@gmail.com>
2023-12-29 20:13:48 +0700

    tambahin halaman detal listing dan detail sampel

---
+++ app/riset/daftar/daftar-listing/detail-listing/page.jsx
@@ -0,0 +1,11 @@
+import Layout from '@/app/layout/layout';
+
+export default function DetailListing() {
+    return(
+        <>
+            <Layout>
+                {/* TODO HERE */}
+            </Layout>
+        </>
+    )
+}
---
+++ app/riset/daftar/daftar-sampel/detail-sampel/page.jsx
@@ -0,0 +1,11 @@
+import Layout from '@/app/layout/layout';
+
+export default function DetailSampel() {
+    return(
+        <>
+            <Layout>
+                {/* TODO HERE */}
+            </Layout>
+        </>
+    )
+}
