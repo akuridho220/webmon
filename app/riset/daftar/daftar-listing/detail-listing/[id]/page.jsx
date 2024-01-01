776bf8a69306778bc88cf0075baaea592c870e42
akuridho220 <akuridho220@gmail.com>
2024-01-01 19:21:25 +0700

    update structur route for detail listing & detail sampel

--- app/riset/daftar/daftar-listing/detail-listing/[id]/page.jsx
+++ app/riset/daftar/daftar-listing/detail-listing/[id]/page.jsx
@@ -1,10 +1,11 @@
 import Layout from '@/app/layout/layout';
 
-export default function DetailSampel() {
+export default function DetailListing({params}) {
     return(
         <>
             <Layout>
                 {/* TODO HERE */}
+                {params.id}
             </Layout>
         </>
     )
--- app/riset/daftar/daftar-sampel/detail-sampel/[id]/page.jsx
+++ app/riset/daftar/daftar-sampel/detail-sampel/[id]/page.jsx
@@ -1,10 +1,11 @@
 import Layout from '@/app/layout/layout';
 
-export default function DetailListing() {
+export default function DetailSampel({params}) {
     return(
         <>
             <Layout>
                 {/* TODO HERE */}
+                {params.id}
             </Layout>
         </>
     )
