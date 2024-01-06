f65ff28c55e96e4d1231ea078defa5fdc85c3774
Alief Zulvan <depanb187@gmail.com>
2024-01-06 11:28:12 +0700

    -style added responsive to daftar listing page --done

--- app/riset/daftar/daftar-listing/components/SelectTable.jsx
+++ app/riset/daftar/daftar-listing/components/SelectTable.jsx
@@ -11,13 +11,13 @@ const SelectTabel = () => {
     setSelectedCategory(selectedValue);
   };
   return (
-    <div className="w-[85%] mx-auto bg-primary-600 rounded-xl shadow-md overflow-auto">
+    <div className="md:w-[85%] w-[95%] mx-auto bg-primary-600 rounded-xl shadow-md overflow-auto">
       <div className="flex w-[95%] mx-auto justify-between items-center">
-        <div className="w-[40%]">
+        <div className="w-[40%] text-xs md:text-base">
           <SelectCat onSelect={handleCategoryChange} />
         </div>
         <div className="">
-          <button className="flex mt-2 items-center justify-center bg-secondary-800 py-2 px-6 w-fit rounded-lg text-white hover:bg-secondary-900">
+          <button className="flex md:mt-2 mt-3 items-center justify-center md:text-base text-xs bg-secondary-800 py-2 px-6 w-fit rounded-lg text-white hover:bg-secondary-900">
             <Icon.Download size={18} />
             <p className=" pl-4">Export</p>
           </button>
