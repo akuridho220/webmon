8261fecf2ec0853ece1230651e506bbf281d7f1d
NUR YUDHA JATI PRAKOSO <222112273@stis.ac.id>
2024-01-18 16:06:19 +0000

    Replace Header.jsx

--- app/riset/daftar/daftar-listing/detail-listing/[kat]/[id]/components/Header.jsx
+++ app/riset/daftar/daftar-listing/detail-listing/[kat]/[id]/components/Header.jsx
@@ -2,26 +2,30 @@ import React, { useState } from 'react';
 import * as Icon from 'react-feather';
 import ModalBS from './detailBS';
 
-export default function Header(){
+export default function Header({data}){
     const [showModalBS, setShowModalBS] = useState(false);
     return(
     <div>
+       <div className="flex items-center justify-center pt-5 pb-3 w-fit rounded-lg text-white">
+            <p>Progress Detail Sampling: </p>
+          </div>
         <div className="flex py-2 w-[100%] justify-between">
           <div className="w-[10%]">
             <button className="flex items-center justify-center bg-secondary-800 py-2 px-6 w-fit rounded-lg text-white hover:bg-secondary-900">
-              <Icon.Download size={18} />
+              <Icon.Download size={20} />
               <p className=" pl-4">Export</p>
             </button>
           </div>
-          <div className="w-[18%]">
+          <div className="w+[10%]">
+          
         <button
           className="flex items-center justify-left bg-secondary-800 py-2 px-6 w-fit rounded-lg text-white hover:bg-secondary-900"
           onClick={() => setShowModalBS(true)}
         >
-          <Icon.Download size={18} />
-          <p className="pl-0">Tentang Blok Sensus</p>
+          <Icon.Info size={20} />
+          <p className="pl-2"> Tentang BS</p>
         </button>
-        {showModalBS && <ModalBS onClosed={() => setShowModalBS(false)} />}
+        {showModalBS && <ModalBS onClosed={() => setShowModalBS(false)} dataBS = {data}  />}
       </div>
     </div>
 </div>
