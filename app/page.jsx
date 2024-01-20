628da9944b0c643436f4bbd6303a5f807b2fde1a
akuridho220 <akuridho220@gmail.com>
2024-01-21 01:36:20 +0700

    update terakhir malem ini harusnya udah bisa build

--- app/page.jsx
+++ app/page.jsx
@@ -1,1 +1,11 @@
-// Ini buat halaman login
+const Tes = () => {
+    return (
+      <>
+        <div className='bg-primary-900'>
+            <p>Halo Ini Webmon</p>
+        </div>
+      </>
+    )
+  }
+  
+  export default Tes;
