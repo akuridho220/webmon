ac9e0427111b018c6230e5348316bd8d0c6457c2
Alief Zulvan <depanb187@gmail.com>
2024-01-28 12:09:36 +0700

    menambahkan status selesai pada dashboard jika kegiatan telah usai

--- app/dashboard/components/Waktu.jsx
+++ app/dashboard/components/Waktu.jsx
@@ -17,10 +17,24 @@ const Waktu = () => {
       const dayEnd = new Date('01/26/2024'); //format bulan / tanggal / tahun
       const timeToDate = dayEnd - today;
 
-      const days = Math.floor(timeToDate / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((timeToDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((timeToDate % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((timeToDate % (1000 * 60)) / 1000);
+      let days = Math.floor(timeToDate / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((timeToDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((timeToDate % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((timeToDate % (1000 * 60)) / 1000);
+
+      // Jika waktu habis
+      if (days < 0) {
+        days = 0;
+      }
+      if (hours < 0) {
+        hours = 0;
+      }
+      if (minutes < 0) {
+        minutes = 0;
+      }
+      if (seconds < 0) {
+        seconds = 0;
+      }
 
       setCountdown({
         days,
@@ -45,17 +59,23 @@ const Waktu = () => {
   return (
     <div className="md:flex block justify-between bg-[#D9D9D9] rounded-md py-2 px-4 space-y-4 md:space-y-0 items-center shadow-md">
       <p className="font-sdr text-2xl text-center md:text-start">{now}</p>
-      <div className="flex justify-center md:justify-start">
-        <p className="text-primary-900 font-semibold">Tersisa</p>
-        <Icon.ArrowRight className="text-primary-900 hidden md:block" />
-        <Icon.ArrowDown className="text-primary-900 block md:hidden" />
-      </div>
-      <div className=" max-md:w-full flex flex-wrap space-x-4 md:justify-start justify-center items-center">
-        <Box text="Hari" angka={countdown.days} />
-        <Box text="Jam" angka={countdown.hours} />
-        <Box text="Menit" angka={countdown.minutes} />
-        <Box text="Detik" angka={countdown.seconds} />
-      </div>
+      {countdown.days === 0 ? (
+        <p className="text-slate-100 px-6 py-2 rounded-md font-semibold text-center bg-primary-800 hover:bg-primary-900">Periode Kegiatan telah Selesai</p>
+      ) : (
+        <div className="flex justify-center md:justify-start">
+          <p className="text-primary-900 font-semibold">Tersisa</p>
+          <Icon.ArrowRight className="text-primary-900 hidden md:block" />
+          <Icon.ArrowDown className="text-primary-900 block md:hidden" />
+        </div>
+      )}
+      {countdown.days > 0 && (
+        <div className="max-md:w-full flex flex-wrap space-x-4 md:justify-start justify-center items-center">
+          <Box text="Hari" angka={countdown.days} />
+          <Box text="Jam" angka={countdown.hours} />
+          <Box text="Menit" angka={countdown.minutes} />
+          <Box text="Detik" angka={countdown.seconds} />
+        </div>
+      )}
     </div>
   );
 };
