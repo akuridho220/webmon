8c45a4419a2caeae4d6964c9cfcfd8f89efd9843
Alief Zulvan <depanb187@gmail.com>
2024-02-23 06:59:47 +0700

    -fix: detail progress tim

--- app/riset/progres/progres-tim/detail/[id]/components/TableDetail.jsx
+++ app/riset/progres/progres-tim/detail/[id]/components/TableDetail.jsx
@@ -1,120 +1,51 @@
-"use client";
-import React from "react";
-import BasicTable from "@/app/components/BasicTable";
-import { Button } from "@nextui-org/react";
+'use client';
+import React from 'react';
+import BasicTable from '@/app/components/BasicTable';
+import { Button } from '@nextui-org/react';
 
-const datas = [
+const TableDetailProgres = ({ data }) => {
+  const columns = [
     {
-      noRuta: "3507040001013B",
-      tim_pencacah: "001",
-      status_cacah: 1,
-      nama_krt: "Juan 1",
+      accessorKey: 'kode_ruta',
+      header: 'Nomor Rumah Tangga',
     },
     {
-      noRuta: "3507040001023B",
-      tim_pencacah: "001",
-      status_cacah: 1,
-      nama_krt: "Juan 2",
+      accessorKey: 'nim_pencacah',
+      header: 'NIM Pencacah',
     },
     {
-      noRuta: "3507040001025B",
-      tim_pencacah: "101",
-      status_cacah: 1,
-      nama_krt: "Juan 3",
+      accessorKey: 'nama_pencacah',
+      header: 'Nama Pencacah',
     },
     {
-      noRuta: "3507040001027B",
-      tim_pencacah: "101",
-      status_cacah: 1,
-      nama_krt: "Egi 1",
+      accessorKey: 'nama_krt',
+      header: 'Nama KRT',
     },
     {
-      noRuta: "3507040001013B",
-      tim_pencacah: "102",
-      status_cacah: 1,
-      nama_krt: "Egi 2",
-    },
-    {
-      noRuta: "3507040001023B",
-      tim_pencacah: "102",
-      status_cacah: 0,
-      nama_krt: "Egi 3",
-    },
-    {
-      noRuta: "3507040001025B",
-      tim_pencacah: "102",
-      status_cacah: 0,
-      nama_krt: "Ridho 1",
-    },
-    {
-      noRuta: "3507040001027B",
-      tim_pencacah: "102",
-      status_cacah: 1,
-      nama_krt: "Ridho 2",
-    },
-    {
-      noRuta: "3507040001013B",
-      tim_pencacah: "103",
-      status_cacah: 0,
-      nama_krt: "Ridho 3",
-    },
-    {
-      noRuta: "3507040001023B",
-      tim_pencacah: "103",
-      status_cacah: 1,
-      nama_krt: "Alip 1",
-    },
-    {
-      noRuta: "3507040001025B",
-      tim_pencacah: "103",
-      status_cacah: 1,
-      nama_krt: "Alip 2",
-    },
-    {
-      noRuta: "3507040001027B",
-      tim_pencacah: "103",
-      status_cacah: 0,
-      nama_krt: "Alip 3",
+      accessorKey: 'status',
+      header: 'Status Cacah',
+      cell: ({ row }) => {
+        return (
+          <div>
+            {row.original.status == 2 ? (
+              <button disabled className="text-green-900 bg-green-200/70 px-4 py-2 rounded-lg">
+                Tercacah
+              </button>
+            ) : (
+              <Button isDisabled color="danger" variant="flat">
+                Belum Tercacah
+              </Button>
+            )}
+          </div>
+        );
+      },
     },
   ];
-
-const TableDetailProgres = ({data}) => {
-    const columns = [
-        {
-            accessorKey: "kode_ruta",
-            header: "Nomor Rumah Tangga",
-        },
-        {
-            accessorKey: "nama_tim",
-            header: "Tim Pencacah",
-        },
-        {
-          accessorKey: "nama_krt",
-          header: "Nama KRT",
-        },
-        {
-          accessorKey: "status",
-          header: "Status Cacah",
-          cell: ({row}) => {
-            return(
-              <div>
-                {row.original.status_cacah==2?
-                  <Button isDisabled color="success" variant="flat">
-                    Tercacah
-                  </Button>:
-                  <Button isDisabled color="danger" variant="flat">
-                    Belum Tercacah
-                  </Button>}
-              </div>
-            )
-          }
-        }
-    ];
-    return(
-        <div>
-            <BasicTable columns={columns} data={data} />
-        </div>
-    )
-}
+  return (
+    <div>
+      <BasicTable columns={columns} data={data} />
+    </div>
+  );
+};
 
 export default TableDetailProgres;
