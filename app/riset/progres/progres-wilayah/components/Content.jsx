d061d37f20ea1b97fe8aeb267dd39fa3ac286ea5
Pandu Wahyu Aji <222112281@stis.ac.id>
2024-02-23 00:43:34 +0700

    update Content

--- app/riset/progres/progres-wilayah/components/Content.jsx
+++ app/riset/progres/progres-wilayah/components/Content.jsx
@@ -20,13 +20,14 @@ const Content = ({ data, listKab }) => (
       <div className="block md:flex justify-between mb-4 pt-6">
         <div className="w-[90%] md:w-fit  justify-start">
           <WilayahSelect />
-          <div onClick={HandleExport} className="flex items-center bg-secondary-800 py-2 px-4 rounded-lg text-white hover:bg-secondary-900">
-            <Icon.Download size={18} />
-            <button className=" pl-4">Export</button>
-          </div>
         </div>
-        <ProgressBar />
-        <TableWilayah />
+        <div onClick={Export} className="mt-3 md:mt-0 flex items-center bg-secondary-800 py-2 px-4 rounded-lg text-white hover:bg-secondary-900">
+          <Icon.Download size={18} />
+          <button className=" pl-4 text-sm md:text-base">Export</button>
+        </div>
+      </div>
+      <ProgressBar />
+      <TableWilayah />
     </div>
   </>
 );
