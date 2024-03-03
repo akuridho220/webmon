5744badc53b3b6a84cd2f8752d2931f70e88f6ae
akuridho220 <akuridho220@gmail.com>
2024-03-03 15:49:45 +0700

    update bug daftar per tim

--- app/riset/daftar/daftar-pertim/page.jsx
+++ app/riset/daftar/daftar-pertim/page.jsx
@@ -17,7 +17,7 @@ const getDataSampel = async () => {
   const [dataSampel, dataListing] = await Promise.all([fetchData(`${apiURL}riset/daftar/tim/sampel`), getDataListing()]);
 
   const mergedData = dataSampel.map((sampelItem) => {
-    const matchingListing = dataListing.find((listingItem) => sampelItem.id_bs === listingItem.id_bs);
+    const matchingListing = dataListing.find((listingItem) => sampelItem.id_tim === listingItem.id_tim);
 
     return {
       ...sampelItem,
